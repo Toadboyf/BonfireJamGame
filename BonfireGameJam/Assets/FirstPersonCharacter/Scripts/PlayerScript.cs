d75287a8abeca5e078c6b0593224bf00eadf4bf2
Toadboyf <Zmartens93@gmail.com>
2020-08-28 17:24:00 -0400

    Importing Kenney Assets

---
+++ BonfireGameJam/Assets/FirstPersonCharacter/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public bool isGrounded;
+    public float moveSpeed;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xMove + transform.forward * zMove;
+
+        controller.SimpleMove(move * moveSpeed);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
